707c9637259ad2f9d4715e4bdc503c1690dfad37
naveenks2002 <90003399+naveenks2002@users.noreply.github.com>
2023-06-22 00:21:20 +0530

    Update 876. Middle of Linked list.cpp

--- Linked_list/876. Middle of Linked list.cpp
+++ Linked_list/876. Middle of Linked list.cpp
@@ -1,1 +1,44 @@
+Brute force approach :
 
+ ListNode* middleNode(ListNode* head) {
+          ListNode* temp;
+          int count=0;
+          temp = head;
+          while(temp!=NULL){
+              count++;
+              temp = temp->next;
+          }
+          int p,q=0;
+          if(count%2==0)
+            p = (count/2) + 1;
+
+          else
+            p = (count+1)/2;
+
+          temp = head;
+          while(temp->next!=NULL){
+              q++;
+              if(q==p)
+                  return temp;
+              temp = temp->next;
+          }
+          return temp;
+    }
+
+
+Optimised approach ( Tortoise-hare method)
+
+  ListNode* middleNode(ListNode* head) {
+          ListNode *fast,*slow;
+          if(head==NULL)
+            return NULL;
+
+          fast = head;
+          slow = head;
+
+          while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+          }
+          return slow;
+    }
