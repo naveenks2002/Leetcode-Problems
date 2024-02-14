58e4e8d4b4009be8e90b39fe8beea98e69ea6953
Naveen Kumar Singh <90003399+naveenks2002@users.noreply.github.com>
2024-02-14 20:36:54 +0530

    Time: 4 ms (81.33%) | Memory: 17.7 MB (60.35%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -18,7 +18,6 @@ public:
         if(root==NULL)  return 0;
         q.push(root);
         while(!q.empty()){
-            vector<int> level;
             int size = q.size();
             for(int i=0; i<size; i++){
                 TreeNode* node = q.front();
